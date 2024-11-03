0e251d5ff513dc50ac6739d739e26968b37fb4db
SwthSawTheme <sawtheme1@gmail.com>
2024-11-03 00:40:23 -0300

    LocalPlayer encontrado

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import json
 # É necessária usar a função builtin da função Open, para não entrar em conflito com a biblioteca pymem
 with __builtins__.open("settings.json","r") as arquivo:
     data = json.load(arquivo)
-    name = data["configs"]["name"]
+    name = data["life"]["name"]
     # convertendo o arquivo carregado para hexadecimal usando o parametro int(arq, 16)
-    endereco = int(data["configs"]["endereco"],16)
+    endereco = int(data["life"]["endereco"],16)
 
     # criando um list comprehension para a conversão de todos as offsets para hexadecimal
-    offsets = [int(f"0x{offset}", 16) for offset in data["configs"]["offsets"]]
+    offsets = [int(f"0x{offset}", 16) for offset in data["life"]["offsets"]]
 
 
 pm = Pymem("SkyrimSE.exe")
