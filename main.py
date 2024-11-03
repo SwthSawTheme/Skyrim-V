758c69344dfe4db00b8fc6ffd1546e0cb99ad5f7
SwthSawTheme <sawtheme1@gmail.com>
2024-11-02 23:43:53 -0300

    Explicando as funçãos e seus usos

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from pymem.process import *
 from pymem.exception import *
 import json
 
+# É necessária usar a função builtin da função Open, para não entrar em conflito com a biblioteca pymem
 with __builtins__.open("settings.json","r") as arquivo:
     data = json.load(arquivo)
     name = data["configs"]["name"]
+    # convertendo o arquivo carregado para hexadecimal usando o parametro int(arq, 16)
     endereco = int(data["configs"]["endereco"],16)
+
+    # criando um list comprehension para a conversão de todos as offsets para hexadecimal
     offsets = [int(f"0x{offset}", 16) for offset in data["configs"]["offsets"]]
 
 
